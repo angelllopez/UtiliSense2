5bde9837fe7260d92dcd725c9bbde86e391ba0a7
A. Lopez <140568825+angelllopez@users.noreply.github.com>
2024-04-24 23:48:37 -0500

    More tests added

--- UtiliSense.Gas.Data/Contracts/IGasConsumptionRepository.cs
+++ UtiliSense.Gas.Data/Contracts/IGasConsumptionRepository.cs
@@ -5,12 +5,12 @@ namespace UtiliSense.Gas.Data.Contracts
     public interface IGasConsumptionRepository : IDisposable
     {
         Task<IEnumerable<GasConsumption>> GetGasConsumptionsAsync();
-        Task<IEnumerable<GasConsumption>> GetGasConsumptionByDayAsync(DateTime date);
+        Task<GasConsumption> GetGasConsumptionByDayAsync(DateTime date);
         Task<IEnumerable<GasConsumption>> GetGasConsumptionByYearAsync(DateTime date);
         Task<IEnumerable<GasConsumption>> GetGasConsumptionByMonthAsync(DateTime date);
-        Task<bool> InsertGasConsumption(GasConsumption gasConsumption);
-        Task<bool> DeleteGasConsumption(int gasConsumptionId);
-        Task<bool> UpdateGasConsumption(GasConsumption gasConsumption);
+        Task<bool> InsertGasConsumptionAsync(GasConsumption gasConsumption);
+        Task<bool> DeleteGasConsumptionAsync(int gasConsumptionId);
+        Task<bool> UpdateGasConsumptionAsync(GasConsumption gasConsumption);
         void Save();
     }
 }
